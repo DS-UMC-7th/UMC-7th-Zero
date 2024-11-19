f6d563a8291a235940f9eefc7b09a00234e50ce2
jiyoung <gyjeong021@gmail.com>
2024-11-20 01:32:27 +0900

    ✨  Feat : Entity 매핑

--- src/main/java/umc/study/domain/FoodCategory.java
+++ src/main/java/umc/study/domain/FoodCategory.java
@@ -3,6 +3,10 @@ package umc.study.domain;
 import jakarta.persistence.*;
 import lombok.*;
 import umc.study.domain.common.BaseEntity;
+import umc.study.domain.mapping.MemberPrefer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,4 +20,7 @@ public class FoodCategory extends BaseEntity {
 
     @Column(nullable = false, length = 15)
     private String name;
+
+    @OneToMany(mappedBy = "foodCategory", cascade =CascadeType.ALL )
+    private List<MemberPrefer> memberPreferList=new ArrayList<>();
 }
--- src/main/java/umc/study/domain/Store.java
+++ src/main/java/umc/study/domain/Store.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 import umc.study.domain.common.BaseEntity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Builder
@@ -21,4 +24,14 @@ public class Store extends BaseEntity {
     private String address;
 
     private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Mission> missionList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
 }
